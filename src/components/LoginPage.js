bfd0ce2e68d94efbbab8023fb058a7acdab0261a
zajaydas <ajayk@sourcesoftsolutions.com>
2021-04-09 18:38:15 +0530

    styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { startLogin } from '../actions/auth';
+
+// export const LoginPage = ({ startLogin }) => (
+//   <div className="box-layout">
+//     <div className="box-layout__box">
+//       <h1 className="box-layout__title">Expensify App</h1>
+//       <p>It's time to get your expenses under control.</p>
+//       <button onClick={startLogin}>Login</button>
+//     </div>
+//   </div>
+// );
+
+// const mapDispatchToProps = (dispatch) => ({
+//   startLogin: () => dispatch(startLogin())
+// });
+
+// export default connect(undefined, mapDispatchToProps)(LoginPage);
+//----------------------------------------------------------------
+
+import React from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <div className="box-layout__title">
+        <h1>Expensify App</h1>
+      </div>
+
+      <p>It's time to get your expenses under control</p>
+      <button className="button" onClick={startLogin}>
+        Login with Google
+      </button>
+    </div>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogin: () => dispatch(startLogin())
+  startLogin: () => dispatch(startLogin()),
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
